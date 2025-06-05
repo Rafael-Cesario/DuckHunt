5a13443687871484eadee48d9eb3342b536861e9
Rafael <RafaelOliveiracds@gmail.com>
2025-06-05 02:16:14 -0300

    Aguardar Inicio

--- DuckHunt.cpp
+++ DuckHunt.cpp
@@ -5,34 +5,60 @@
 #define JANELA_X 640
 #define JANELA_Y 480
 
-void gerarInterfaceInicial();
-void tamparAlvo();
-void exibirVidas(int vidas);
-void exibirMunicao(int municao);
-void exibirAcertos(int acertos);
+typedef struct
+{
+    char caminho[22];
+    int x, y;
+} Elemento;
 
+void gerarInterfaceInicial();
 void timer();
+void exibirAcertos(int acertos);
+void exibir(Elemento item, int quantidade);
+void aguardarInicio();
 
 int main ()
 {
-    initwindow(JANELA_X, JANELA_Y, "Duck Hunt", 600, 100);
+    initwindow(JANELA_X, JANELA_Y, "Duck Hunt", 100, 100);
 
     gerarInterfaceInicial();
-    // tamparAlvo();
-    // iniciarJogo();
+    aguardarInicio();
 
-    tamparAlvo();
-    timer();
-
-    // Fecha a janela quando uma tecla for pressionada
     getch();
     closegraph();
 
     return 0;
 }
 
+void aguardarInicio()
+{
+    int clickX, clickY;
+
+    while (1)
+    {
+        // Mantem o jogo parado, esperando por um clique
+        while(!ismouseclick(WM_LBUTTONDOWN)) delay(10);
+
+        // Salva a posição do clique
+        getmouseclick(WM_LBUTTONDOWN, clickX, clickY);
+        clearmouseclick(WM_LBUTTONDOWN);
+
+        // Verifica se o clique foi dentro do alvo
+        if (clickX >= 291 && clickX <= 341 && clickY >= 190 && clickY <= 240)
+        {
+            // Tampa a mensagem inicial e o alvo
+            setfillstyle(1, BLACK);
+            bar(224, 161, 224 + 180, 161 + 100);
+            break;
+        }
+    }
+}
+
 void gerarInterfaceInicial()
 {
+    Elemento vida = {"Imagens/heart.jpg", 16, 393};
+    Elemento municao = {"Imagens/bullet.jpg", 16, 430};
+
     // Timer
     setfillstyle(1, COLOR(20,20,20));
     bar(0, 0, JANELA_X, 30);
@@ -41,84 +67,16 @@ void gerarInterfaceInicial()
 
     // Alvo para iniciar o jogo
     setbkcolor(BLACK);
-    outtextxy(224, 161, "Acerte o alvo para comeï¿½ar");
+    outtextxy(224, 161, "Acerte o alvo para começar");
     readimagefile("Imagens/bullseye.jpg", 291, 190, 291 + 50, 190 + 50);
 
     // Background na parte inferior
     setfillstyle(1, COLOR(20,20,20));
     bar(0, 370, JANELA_X, 370 + 110);
 
-    exibirVidas(5);
-    exibirMunicao(5);
+    exibir(vida, 5);
+    exibir(municao, 5);
     exibirAcertos(0);
-}
-
-
-void tamparAlvo()
-{
-    int x,y;
-
-    //esconde o alvo
-
-    while(1)
-    {
-        if(ismouseclick(WM_LBUTTONDOWN))
-        {
-
-
-            getmouseclick(WM_LBUTTONDOWN,x,y);
-
-            if(x>=291&& x<=341 && y>=190 && y<=240)
-            {
-                setfillstyle(SOLID_FILL,BLACK);
-                bar(224,161,200+224,100+161);
-
-                setbkcolor(BLACK);
-                outtextxy(250,161,"O jogo vai comeï¿½ar");
-
-                delay(2000);
-
-                //apaga a mensagem "o jogo vai comeï¿½ar"
-
-                bar(250,161,250+150,161+150);
-            }
-
-            break;
-
-        }
-    }
-
-}
-
-void exibirVidas(int vidas)
-{
-    int i, x;
-
-    for (i = 0; i < vidas; i++)
-    {
-        x = 16 + (i * 32);
-        readimagefile("Imagens/heart.jpg", x, 393, x + 32, 393 + 32);
-    }
-}
-
-void exibirMunicao(int municao)
-{
-    int i, x;
-
-    for (i = 0; i < municao; i++)
-    {
-        x = 16 + (i * 32);
-        readimagefile("Imagens/bullet.jpg", x, 430, x + 32, 430 + 32);
-    }
-}
-
-void exibirAcertos(int acertos)
-{
-    char text[11];
-
-    sprintf(text, "Acertos: %d", acertos);
-    setbkcolor(COLOR(20, 20, 20));
-    outtextxy(552, 447, text);
 }
 
 void timer()
@@ -142,4 +100,22 @@ void timer()
 
     setbkcolor(BLACK);
     outtextxy(280,161,"Fim de jogo");
+}
+
+void exibir(Elemento item, int quantidade)
+{
+    int x, i = 0;
+    for (i = 0; i < quantidade; i++)
+    {
+        x = item.x + (i * 32);
+        readimagefile(item.caminho, x, item.y, x + 32, item.y + 32);
+    }
+}
+
+void exibirAcertos(int acertos)
+{
+    char text[11];
+    sprintf(text, "Acertos: %d", acertos);
+    setbkcolor(COLOR(20, 20, 20));
+    outtextxy(552, 447, text);
 }
